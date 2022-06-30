2abf9af70b7c6b84ede80b0f400ac3996f775b4a
Rowan <malice.peanutbutter@gmail.com>
2022-06-30 20:40:42 +0200

    Now using classes. Test page added.

--- content.js
+++ content.js
@@ -1,39 +1,67 @@
 console.log({ document });
 
-let numberOfClicks = 0;
-document.body.addEventListener("click", () => {
-  numberOfClicks++;
-  renderPopup();
-});
+// -------------------------------------------------
+class words {
+  getAllWordsInDocument() {
+    let thingsPopup = document.getElementById("things-popup");
+    if (!thingsPopup) return [];
 
-var totalMouseMoveDistance = 0;
-var lastSeenAt = { x: null, y: null };
-document.addEventListener("mousemove", (e) => {
-  if (lastSeenAt.x) {
-    totalMouseMoveDistance += Math.sqrt(
-      Math.pow(lastSeenAt.y - e.clientY, 2) +
-        Math.pow(lastSeenAt.x - e.clientX, 2)
-    );
+    // TODO: Wow this is super hacky. Get a better way.
+    let everything = document.body.innerText.replace(thingsPopup.innerText, "");
+
+    // TODO: This is actually a little off.
+    return everything
+      .replaceAll(",", " ")
+      .replaceAll("\n", " ")
+      .trim()
+      .split(" ");
   }
-  lastSeenAt.x = e.clientX;
-  lastSeenAt.y = e.clientY;
-  renderPopup();
-});
+}
 
-let totalOffset = 0;
-let currOffset = window.pageYOffset;
-window.addEventListener("scroll", () => {
-  let addedOffset = Math.abs(currOffset - window.pageYOffset);
-  totalOffset += addedOffset;
+class mouse {
+  numberOfClicks = 0;
+  totalMouseMoveDistance = 0;
+  lastSeenAt = { x: null, y: null };
+  totalOffset = 0;
   currOffset = window.pageYOffset;
-  renderPopup();
-});
+
+  monitor() {
+    document.addEventListener("click", () => {
+      this.numberOfClicks++;
+      renderPopup();
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      if (this.lastSeenAt.x) {
+        this.totalMouseMoveDistance += Math.sqrt(
+          Math.pow(this.lastSeenAt.y - e.clientY, 2) +
+            Math.pow(this.lastSeenAt.x - e.clientX, 2)
+        );
+      }
+      this.lastSeenAt.x = e.clientX;
+      this.lastSeenAt.y = e.clientY;
+
+      renderPopup();
+    });
+
+    document.addEventListener("scroll", () => {
+      let addedOffset = Math.abs(this.currOffset - window.pageYOffset);
+      this.totalOffset += addedOffset;
+      this.currOffset = window.pageYOffset;
+      renderPopup();
+    });
+  }
+}
+
+let wordThings = new words();
+let mouseThings = new mouse();
+mouseThings.monitor();
 
 function renderItemsToShow(itemsToShow) {
   return (
     "<div>" +
     Object.entries(itemsToShow)
-      .map(([key, value]) => `<div>${key}: ${value}</div>`)
+      .map(([key, value]) => `<div>${key}: ${value}</div> `)
       .join("") +
     "</div>"
   );
@@ -57,12 +85,14 @@ let renderPopup = () => {
       (window.innerWidth ||
         document.documentElement.clientWidth ||
         document.body.clientWidth) + "px",
+    "Number of words on Page": wordThings.getAllWordsInDocument().length,
+    "Words on this page": "a",
   };
 
   let thingsIHaveDone = {
-    Clicks: numberOfClicks,
-    Scrolled: Math.round(totalOffset) + "px",
-    "Mouse moved": Math.round(totalMouseMoveDistance) + "px",
+    Clicks: mouseThings.numberOfClicks,
+    Scrolled: Math.round(mouseThings.totalOffset) + "px",
+    "Mouse moved": Math.round(mouseThings.totalMouseMoveDistance) + "px",
   };
 
   let container = document.getElementById("things-popup");
@@ -75,7 +105,7 @@ let renderPopup = () => {
   container.innerHTML =
     "<div class='title'>Things On This Page</div>" +
     renderItemsToShow(thingsOnThisPage) +
-    "<br />" +
+    "<br /> " +
     "<div class='title'>Things You Have Done</div>" +
     renderItemsToShow(thingsIHaveDone);
   document.body.appendChild(container);
