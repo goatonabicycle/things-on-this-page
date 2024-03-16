d169abea0d2ef2f28a0d9f275fd8bf892edf4c53
Rowan <malice.peanutbutter@gmail.com>
2024-03-16 23:10:32 +0200

    Added release ts. Just thinking for now.

---
+++ release.ts
@@ -0,0 +1,4 @@
+// This will be a script that automatically release this extension all the way to the Chrome Web Store.
+// I should find a package or just handle it myself.
+// It should be a fairly simple process initially. Zip the files, upload them to the Chrome Web Store, and then publish the extension.
+// Later I could automate version increases and other things.
