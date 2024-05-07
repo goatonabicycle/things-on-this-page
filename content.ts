acd51a2ba89eea0e804cbab74f138157778ad4b8
Rowan <malice.peanutbutter@gmail.com>
2024-05-07 21:47:52 +0200

    Ignore flags for now

--- content.ts
+++ content.ts
@@ -5,8 +5,9 @@ import { thingsPopup } from "./content/things-popup";
 import { timeCounter } from "./content/time-counter";
 import { setFlags, isPanelVisible, Flags } from "./content/feature-flags";
 
-const mouseActive = isPanelVisible("mouse");
-if (mouseActive) mouse.monitor();
+// const mouseActive = isPanelVisible("mouse");
+// if (mouseActive)
+mouse.monitor();
 
 function updateEverySecond(): void {
   thingsPopup.renderThingsSection();
