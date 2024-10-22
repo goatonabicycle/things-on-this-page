ef97dd3b036651fec0e1f9224fdf31cc988d9388
Rowan <malice.peanutbutter@gmail.com>
2024-10-22 18:35:04 +0200

    Now using the extension js project

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { crx } from "@crxjs/vite-plugin";
-
-import { manifest } from "./manifest.ts";
-
-export default defineConfig({
-  plugins: [crx({ manifest })],
-});
